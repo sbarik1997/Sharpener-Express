d097aba12d51f4d45e6bfd814848f8c83b22cbad
supriya <supriyabarik116@gmail.com>
2024-02-18 11:22:42 +0530

    middlewares in express

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req,res,next) => {
-    console.log("In the middleware!");
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next) => {
+    res.send('<form action="/product" method="post"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req,res,next) => {
-    console.log("In an another middleware!");
+app.post('/product', (req,res) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next) => {
     res.send('<h1>Hello from server!</h1>');
 });
 
