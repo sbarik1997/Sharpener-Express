421e052d4aec95d6be3584a0ca1e46e0f4e5379f
supriya <supriyabarik116@gmail.com>
2024-03-08 21:54:45 +0530

    dynamic routes part 1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views', path.resolve('views'));
 
 const chatRouter = require('./routes/chatApp');
 const adminRouter = require('./routes/admin');
--- controllers/error404.js
+++ controllers/error404.js
@@ -1,7 +1,3 @@
-const path = require('path');
-
-const rootDir = require('../utils/path');
-
-exports.errorPage = (req,res,next) => {
-    res.status(404).render('404');
-}
+exports.errorPage = (req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
+  };
